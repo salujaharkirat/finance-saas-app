f13467c7f7650eac0fed5aca25ad01e8ec720676
Harkirat <saluja.harkirat@gmail.com>
2025-04-22 14:02:58 +0530

    feat: removed unused code from dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,32 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
-    const accountsQuery = useGetAccounts();
-    const { onOpen } = useNewAccount();
-
-    if (accountsQuery.isLoading) {
-        return (
-            <div>
-                Loading...
-            </div>
-        )
-    }
-
-    return (
-        <>
-            <Button onClick={onOpen}>Add</Button>
-                <div>
-                {accountsQuery.data?.data?.map((account) => (
-                <div key={account.id}>
-                    {account.name}
-                </div>
-            ))}
-        </div>
-        </>
-
-    )
+    return null;
 }
