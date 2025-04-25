264bcfb0e7965ed9ea476b3807eb66855a310fc6
Harkirat <saluja.harkirat@gmail.com>
2025-04-25 19:53:59 +0530

    feat: summary api + cards ui

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,37 @@
-"use client";
+import { DataGrid } from "@/components/data-grid";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 
-export default function Home() {
-    return null;
+const DashboardInnerPage = () => {
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <DataGrid />
+        </div>
+    )
 }
+
+const DashboardPage = () => {
+    return (
+      <Suspense fallback={
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+          <Card className="border-none drop-shadow-sm">
+            <CardHeader>
+              <Skeleton className="h-8 w-48"/>
+            </CardHeader>
+            <CardContent>
+              <div className="h-[500px] w-full flex items-center justify-center">
+                <Loader2 className="size-6 text-slate-300 animate-spin"/>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      }>
+        <DashboardInnerPage />
+      </Suspense>
+    );
+  }
+
+  export default DashboardPage;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import accounts from "./accounts";
 import categories from "./categories";
 import { HTTPException } from "hono/http-exception";
 import transactions from "./transactions";
+import summary from "./summary";
 
 
 export const runtime = "edge";
@@ -24,7 +25,8 @@ app.onError((error, ctx) => {
 const routes = app
     .route("/accounts", accounts)
     .route("/categories", categories)
-    .route("/transactions", transactions);
+    .route("/transactions", transactions)
+    .route("/summary", summary);
 
 
 export const GET = handle(app);
